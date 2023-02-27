7f7780b1831df3422ab34f90db31661f684c6d83
Ananya <am404@students.ubc.ca>
2023-02-26 17:43:21 -0800

    Specified all UI methods except last 4

--- src/main/ui/WeeklyPlanner.java
+++ src/main/ui/WeeklyPlanner.java
@@ -22,6 +22,9 @@ public class WeeklyPlanner {
 
     private Scanner input;
 
+    // EFFECTS: creates a new WeeklyPlanner with all the days of the week, a pantry, a shopping list, an empty list of
+    //          meals for the week, and a scanner
+    //          then runs the primary UI method
     public WeeklyPlanner() {
         sunday = new Day("Sunday");
         monday = new Day("Monday");
@@ -38,6 +41,8 @@ public class WeeklyPlanner {
         runWeeklyPlanner();
     }
 
+    // EFFECTS: takes user input and keeps running the program
+    //          if the user inputs "7", ends the program
     private void runWeeklyPlanner() {
         boolean loop = true;
 
@@ -56,7 +61,23 @@ public class WeeklyPlanner {
         System.out.println("Have a healthy, nutritious day!");
     }
 
+    // EFFECTS: displays a menu of options that the user can choose from
+    private void displayMenu() {
+        System.out.println("\nWhat would you like to do? Please note that your pantry refers to the ingredients "
+                + "you already have at home");
+        System.out.println("\t1: Add a meal to a day of the week");
+        System.out.println("\t2: Add an ingredient to your pantry");
+        System.out.println("\t3: Remove an ingredient from your pantry");
+        System.out.println("\t4: View your pantry");
+        System.out.println("\t5: View your shopping list");
+        System.out.println("\t6: View your weekly meal planner");
+        System.out.println("\t7: Quit application");
+    }
 
+
+    // EFFECTS: only runs if user inputs anything other than "7"
+    //          if the user inputs a number from 1-6, executes the respective required function
+    //          otherwise, requests the user to only enter valid inputs
     private void doThing(String whatToDo) {
         if (whatToDo.equals("1")) {
             doAddingMealToDay();
@@ -75,18 +96,14 @@ public class WeeklyPlanner {
         }
     }
 
-    private void displayMenu() {
-        System.out.println("\nWhat would you like to do? Please note that your pantry refers to the ingredients "
-                + "you already have at home");
-        System.out.println("\t1: Add a meal to a day of the week");
-        System.out.println("\t2: Add an ingredient to your pantry");
-        System.out.println("\t3: Remove an ingredient from your pantry");
-        System.out.println("\t4: View your pantry");
-        System.out.println("\t5: View your shopping list");
-        System.out.println("\t6: View your weekly meal planner");
-        System.out.println("\t7: Quit application");
-    }
 
+    // MODIFIES: Day, mealsForTheWeek, shoppingList
+    // EFFECTS: if input is valid and not "q":
+    //              begins the process of adding a meal to the day desired by the user,
+    //              adds the meal the list of all meals for the week,
+    //              and adds required ingredients to the shopping list
+    //          if user input is "q", quits the method
+    //          otherwise, asks the user to enter valid input
     private void doAddingMealToDay() {
         Boolean loop = true;
         while (loop) {
@@ -96,11 +113,11 @@ public class WeeklyPlanner {
             if (day.equals("sun") || day.equals("mon") || day.equals("tue") || day.equals("wed") || day.equals("thu")
                     || day.equals("fri") || day.equals("sat")) {
                 Meal m = whichMealToAdd();
+                addMealToSpecificDay(day, m);
                 mealsForTheWeek.add(m);
                 for (Ingredient i: m.getIngredients()) {
                     addToShoppingList(i);
                 }
-                addMealToSpecificDay(day, m);
                 loop = false;
             } else if (day.equals("q")) {
                 loop = false;
@@ -108,6 +125,8 @@ public class WeeklyPlanner {
         }
     }
 
+    // MODIFIES: Day
+    // EFFECTS: adds the given meal to the list of meals for the day with the given name
     private void addMealToSpecificDay(String day, Meal meal) {
         if (day.equals("sun")) {
             sunday.addMeal(meal);
@@ -126,6 +145,8 @@ public class WeeklyPlanner {
         }
     }
 
+    // MODIFIES: Meal
+    // EFFECTS: creates and returns a meal according to the name, meal type and ingredient list inputted by user
     private Meal whichMealToAdd() {
         System.out.println("Please enter the name of the meal you would like to add.");
         String mealName = input.nextLine();
@@ -143,28 +164,9 @@ public class WeeklyPlanner {
         return meal;
     }
 
-    private void addIngredientsToMeal(Meal m) {
-        Ingredient i = createIngredient();
-        m.addIngredient(i);
-        //addToShoppingList(i);
-
-        Boolean loop = true;
-
-        while (loop) {
-            System.out.println("Do you want to add more ingredients? Type 'y' for yes or anything else for no.");
-            //input.nextLine();
-            String addMore = input.nextLine();
-            addMore = addMore.toLowerCase();
-            if (addMore.equals("y")) {
-                Ingredient moreIngredient = createIngredient();
-                m.addIngredient(moreIngredient);
-               // addToShoppingList(moreIngredient);
-            } else {
-                loop = false;
-            }
-        }
-    }
-
+    // MODIFIES: Meal
+    // EFFECTS: sets the type of the meal according to the input if valid
+    //          otherwise, requests user to enter valid input
     private String doSettingType(String s) {
         Boolean loop = true;
         String returnValue = "";
@@ -189,23 +191,64 @@ public class WeeklyPlanner {
         return returnValue;
     }
 
+    // MODIFIES: Meal
+    // EFFECTS: adds one or more user-created ingredients to the given meal
+    private void addIngredientsToMeal(Meal m) {
+        Ingredient i = createIngredient();
+        m.addIngredient(i);
+        //addToShoppingList(i);
+
+        Boolean loop = true;
+
+        while (loop) {
+            System.out.println("Do you want to add more ingredients? Type 'y' for yes or anything else for no.");
+            String addMore = input.nextLine();
+            addMore = addMore.toLowerCase();
+            if (addMore.equals("y")) {
+                Ingredient moreIngredient = createIngredient();
+                m.addIngredient(moreIngredient);
+               // addToShoppingList(moreIngredient);
+            } else {
+                loop = false;
+            }
+        }
+    }
+
+    // EFFECTS: creates an Ingredient object according to the user inputted name and quantity
+    private Ingredient createIngredient() {
+        System.out.println("Please enter the name of the ingredient you want to add or remove");
+        String ingredientName = input.nextLine();
+        System.out.println("Please enter the quantity of this ingredient you want to add or remove");
+        double ingredientQuantity = input.nextDouble();
+        input.nextLine();
+        Ingredient ingredient = new Ingredient(ingredientName, ingredientQuantity);
+        return ingredient;
+    }
+
+    // MODIFIES: Pantry
+    // EFFECTS: adds user created ingredient to pantry
     private void doAddingIngredientToPantry() {
         addToPantry(createIngredient());
 
     }
 
+    // MODIFIES: Pantry
+    // EFFECTS: removes user desired ingredient to pantry
     private void doRemovingIngredientFromPantry() {
         removeFromPantry(createIngredient());
     }
 
+    // EFFECTS: displays all the ingredients in the pantry
     private void doViewingPantry() {
         System.out.println(pantry.printableIngredientList("The ingredients you already have are: "));
     }
 
+    // EFFECTS: displays all the ingredients in the shopping list
     private void doViewingShoppingList() {
         System.out.println(shoppingList.printableIngredientList("Your shopping list is for the week is: "));
     }
 
+    // EFFECTS: displays all the meals planned for each day of the week and displays their ingredients if user desires
     private void doViewingWeeklyPlanner() {
         System.out.println("Sunday: " + sunday.printableMealList());
         System.out.println("Monday: " + monday.printableMealList());
@@ -215,6 +258,13 @@ public class WeeklyPlanner {
         System.out.println("Friday: " + friday.printableMealList());
         System.out.println("Saturday: " + saturday.printableMealList());
 
+        viewIngredientsForMeal();
+
+    }
+
+    // EFFECTS: if user desires to view the ingredients for a meal, displays them
+    //          otherwise, quits method
+    private void viewIngredientsForMeal() {
         Boolean loop = true;
         while (loop) {
             System.out.println("If you would like to view the ingredients for a particular meal, type its name "
@@ -231,15 +281,17 @@ public class WeeklyPlanner {
         }
     }
 
-
-    private Ingredient createIngredient() {
-        System.out.println("Please enter the name of the ingredient you want to add or remove");
-        String ingredientName = input.nextLine();
-        System.out.println("Please enter the quantity of this ingredient you want to add or remove");
-        double ingredientQuantity = input.nextDouble();
-        input.nextLine();
-        Ingredient ingredient = new Ingredient(ingredientName, ingredientQuantity);
-        return ingredient;
+    // EFFECTS: returns the total quantity of the given ingredient that is required for that week's planned meals
+    private double quantityOfIngredientRequiredForWeek(Ingredient inputIngredient) {
+        double quantity = 0;
+        for (Meal m: mealsForTheWeek) {
+            for (Ingredient mealIngredient: m.getIngredients()) {
+                if (mealIngredient.getName().equals(inputIngredient.getName())) {
+                    quantity += mealIngredient.getQuantity();
+                }
+            }
+        }
+        return quantity;
     }
 
     private void addToShoppingList(Ingredient i) {
@@ -267,6 +319,7 @@ public class WeeklyPlanner {
         removeFromShoppingList(i);
     }
 
+    // REQUIRES: the given ingredient must exist in the pantry
     private void removeFromPantry(Ingredient i) {
         Ingredient pantryIngredient = pantry.getSpecificIngredient(i);
         double quantityOfIngredientRequiredForWeek = quantityOfIngredientRequiredForWeek(i);
@@ -294,17 +347,7 @@ public class WeeklyPlanner {
         }
     }
 
-    private double quantityOfIngredientRequiredForWeek(Ingredient inputIngredient) {
-        double quantity = 0;
-        for (Meal m: mealsForTheWeek) {
-            for (Ingredient mealIngredient: m.getIngredients()) {
-                if (mealIngredient.getName().equals(inputIngredient.getName())) {
-                    quantity += mealIngredient.getQuantity();
-                }
-            }
-        }
-        return quantity;
-    }
+
 
 
 }
